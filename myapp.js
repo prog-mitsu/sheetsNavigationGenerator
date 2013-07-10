81a6d495e972a430030b435fb8a57342507395aa
ms32 <loopa.mix@gmail.com>
2013-07-10 15:36:14 +0900

    first step

---
+++ myapp.js
@@ -0,0 +1,139 @@
+var MYAPP = {
+    // スプレッドシートへのアクセスキー
+    SPREADSHEET_KEY: "表示したいスプレッドシートのkey",
+    // アクセスしたいシート名
+    TARGET_SHEET_NAME: "表示したいシート",
+    // スプレッドシートへアクセスするベースURL
+    BASE_URL: "http://spreadsheets.google.com/tq?key=",
+    // jqGrid表示DOM名
+    JQGRID_TARGET_DOM_NAME: "#jqGridList",          // for sample
+    
+    // navigationシートから取得した各シートのURL情報
+    navigationDict: {},
+    
+    /**--------------------------------------------
+    * Navigationシート内容取得コールバック適用
+    *---------------------------------------------*/
+    sendNavigationSheetCallback: function(){
+        "use strict";
+        
+        // Navigationシートは先頭固定のため「gid=0」にしています
+        var url   = MYAPP.BASE_URL + MYAPP.SPREADSHEET_KEY + "&gid=0&pub=0",
+            query = new google.visualization.Query(url);
+            
+        query.send(MYAPP.runNavigationSheetCallback); 
+    },
+    
+    /**--------------------------------------------
+    * Navigationシート内容取得コールバック実行
+    * @param  {response}  google.visualization.Query 実行後の返り値
+    * Navigationシートから各シートへのURL情報を取得
+    *---------------------------------------------*/
+    runNavigationSheetCallback: function(response){
+        "use strict";
+        
+        var data = response.getDataTable(),
+            url, row, rowLength, key, keyStr, value, query;
+            
+        console.log("spreadsheetHandleResponseDefault start");
+        if (!data || response.isError()) {
+            alert(response.getMessage() + ':' + response.getDetailedMessage());
+            return;
+        }        
+        // 行単位にデータを読み込んでシート情報を取得
+        for (row = 0, rowLength = data.getNumberOfRows(); row < rowLength; row++){
+            keyStr = String(data.getFormattedValue(row, 0));
+            value  = data.getFormattedValue(row, 1);
+            MYAPP.navigationDict[keyStr] = value;
+        }
+        console.log(this.spreadsheetInfoPageDict);
+        
+        // 指定したgoogleスプレッドシートを読み込む
+        url = MYAPP._getSpreadsheetUrl(MYAPP.TARGET_SHEET_NAME);
+        if(url){
+            query  = new google.visualization.Query(url);
+            query.send(MYAPP.runTargetSheetCallback);
+        }
+        console.log("spreadsheetHandleResponseDefault end");
+    },
+    
+    /**--------------------------------------------
+    * 取得対象シート内容取得コールバック実行
+    * @param  {response}  google.visualization.Query 実行後の返り値
+    * データを読み込みたいシートから内容取得
+    *---------------------------------------------*/
+    runTargetSheetCallback: function(response){
+        "use strict";
+        
+        var DISP_ROW_MAX = 100,
+            data = response.getDataTable(),
+            value, row, col, rowLength, colLength, colName, rowTempDict,
+            colArray = [], rowArray = [];
+            
+        if (!data || response.isError()) {
+            alert(response.getMessage() + ':' + response.getDetailedMessage());
+            return;
+        }
+        // loop max cache
+        rowLength = data.getNumberOfRows();
+        colLength = data.getNumberOfColumns();
+        
+        // ヘッダ読み込み(列データ)
+        for ( col = 0; col < colLength; col += 1) {
+            value = data.getColumnLabel(col);
+            colArray.push({ "name" : value, "label" : value,
+                "sortable" : true, "width" : 100
+            });
+        }
+               
+        // 行データ読み込み,格納
+        for ( row = 0; row < rowLength; row += 1) {
+            rowTempDict = [];
+            for ( col = 0; col < colLength; col += 1) {
+                value   = data.getValue(row, col);          // 指定位置のデータ取得
+                colName = data.getColumnLabel(col);         // 格納位置割り出し
+                rowTempDict[colName] = value;               // 行データの1要素格納
+            }
+            rowArray.push(rowTempDict);                     // 行データ格納
+        }
+        
+        // jqGrid表示設定
+        jQuery(MYAPP.JQGRID_TARGET_DOM_NAME).jqGrid({
+            data: rowArray,
+            colModel: colArray,
+            datatype: "local",
+            height: 'auto',
+            sortorder: "ASC",
+            multiselect: false,
+            rowNum:DISP_ROW_MAX,
+            excel: true,
+            viewrecords: true,
+            caption: "[" + MYAPP.TARGET_SHEET_NAME + "] の内容"
+        });
+    },
+
+    /**- - - - - - - - - - - - - - - - - - - - - - -
+    * 指定したシート名のURLを返す
+    * @param  {sheetName} スプレッドシートのシート名
+    * @return {String}    URL文字列(見つからない場合はnull)
+    *- - - - - - - - - - - - - - - - - - - - - - -*/
+    _getSpreadsheetUrl: function(sheetName){
+        
+        "use strict";
+        var url, pageId;
+        
+        // 対象のシート名からシートID(gid)を取得してURLを生成する
+        if( sheetName in MYAPP.navigationDict ){
+            pageId = String(MYAPP.navigationDict[sheetName]);
+            url    = MYAPP.BASE_URL + MYAPP.SPREADSHEET_KEY + 
+                    "&gid=" + pageId + "&pub=0";
+            return url;
+        }
+        alert("ERROR:指定したスプレッドシートの名前が見つかりません (NAME:" + sheetName + ")");
+        return null;
+    }
+    
+};
+
+
+
---
+++ sheetsNavigationGenerator.gs
@@ -0,0 +1,95 @@
+/**
+ * googleスプレッドシートの各種シートに、javascriptからアクセスするための<br>
+ * ナビゲーション情報シートを生成します<br>
+ * @author ms32
+ * @version 1.0.0
+ * 
+ */
+
+var spreadsheetWriteInfo = {
+    // メニュー表示文字列（トップメニュー）
+    MENU_STR_TOP:        "sheetsNavigationGenerator",
+    // メニュー表示文字列（サブメニュー）
+    MENU_STR_SUB:        "run",
+    
+    /**
+     * 全シートのgidを収集して、スプレッドシートの先頭シートに書き込みます
+     * @param {spreadsheetObj} 対象スプレッドシート
+     */
+    create: function(spreadsheetObj){
+        "use strict";
+        
+        var WRITE_ROW_START_POS = 1;        // シート内の書き込み開始位置(row)
+        var WRITE_COL_START_POS = 1;        // シート内の書き込み開始位置(column)
+        
+        var spreadsheetId = spreadsheetObj.getId(), 
+            spreadsheetURL = spreadsheetObj.getUrl(), 
+            spreadsheetName = spreadsheetObj.getName(), 
+            sheets = spreadsheetObj.getSheets(), 
+            i = 0, iLength = 0, 
+            sheetObj = null, range = null, 
+            writeArray = [];
+
+        Logger.log("spreadsheetWriteInfo start");
+        Logger.log("spreadsheetId   = " + spreadsheetId);
+        Logger.log("spreadsheetURL  = " + spreadsheetURL);
+        Logger.log("spreadsheetName = " + spreadsheetName);
+        
+        // 先頭には、このスプレッドシートにアクセスするためのkeyを書き込んでおきます
+        writeArray.push(["key", spreadsheetId]);
+        
+        // 2列目以降には、各シートのシート名とポインタ情報を書き込みます
+        for (i = 0, iLength = sheets.length; i < iLength; i+=1) {
+            sheetObj = sheets[i];
+            Logger.log(sheetObj.getName());
+            Logger.log(sheetObj.getSheetId());
+            Logger.log(sheetObj.getIndex());
+            writeArray.push([sheetObj.getName(), sheetObj.getSheetId()]);  // 名前とシートID(gid)のペアを出力
+        }
+
+        // 集めた内容を先頭シートに出力
+        sheetObj = sheets[0];
+        sheetObj.clear();
+        range = sheetObj.getRange(WRITE_ROW_START_POS, WRITE_COL_START_POS, writeArray.length, writeArray[0].length);
+        range.setValues(writeArray);
+        Logger.log("spreadsheetWriteInfo end"); 
+    },
+    
+    /**
+     * グローバルコールバックのonOpenから呼び出されます
+     * メニュー表示と関数呼び先を設定します
+     */
+    onOpen: function(spreadsheetObj){
+        "use strict";
+
+        var menuList = [];
+        menuList.push({
+            name : spreadsheetWriteInfo.MENU_STR_SUB,
+            functionName : "onMenuRun"
+        });
+        spreadsheetObj.addMenu(spreadsheetWriteInfo.MENU_STR_TOP, menuList); 
+        
+    }
+    
+};
+
+
+/**
+ * システムから呼ばれるグローバルコールバック 
+ * シートオープン時に呼ばれる
+ */
+function onOpen() {
+    "use strict";
+   var spreadsheetObj = SpreadsheetApp.getActiveSpreadsheet();
+   spreadsheetWriteInfo.onOpen(spreadsheetObj);
+}
+
+/**
+ * メニュー実行から呼ばれるグローバルコールバック 
+ * メニュー選択時に呼ばれる
+ */
+function onMenuRun() {
+    "use strict";
+    var spreadsheetObj = SpreadsheetApp.getActiveSpreadsheet();
+    return spreadsheetWriteInfo.create(spreadsheetObj);
+}
